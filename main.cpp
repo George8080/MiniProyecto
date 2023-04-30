50951b7a975575fd4c1312b680a45fddc3da07a6
daninonino <danielanovoao600@gmail.com>
2023-04-29 20:53:27 -0400

    implementacion de main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,58 @@
+#include <iostream>
 #include "ListArr.h"
+#include <chrono>
+#include <random>
 
 using namespace std;
 
 int main(){
+//	random_device rd;
+
+	const unsigned long long n = 10;
+	const long long maxRandValue = 2;
+	double total_time=0;
+	
+	ListArr *la = new ListArr(20);
+
+	cout << "\nInsert left: \n" << endl;
+	for(unsigned long long i=0; i<n ; ++i){
+		auto start_time= chrono::high_resolution_clock::now();
+		la->insert_left(5);
+		auto end_time = chrono::high_resolution_clock::now();
+		auto duration_time = chrono::duration_cast<chrono::microseconds>(end_time - start_time).count();
+		total_time += duration_time;
+
+	}
+
+	cout << "Tiempo total de la operacion: " << total_time << "segundos" << endl;
+	cout << "Tiempo promedio de la operacion: " << total_time/n << "segundos" << endl;
+
+	cout << "\nInsert right: " << endl;
+	for(unsigned long long i=0; i<n ; ++i){
+		auto start_time= chrono::high_resolution_clock::now();
+		la->insert_right(5);
+		auto end_time = chrono::high_resolution_clock::now();
+		auto duration_time = chrono::duration_cast<chrono::microseconds>(end_time - start_time).count();
+		total_time += duration_time;
+
+	}
+
+	cout << "Tiempo total de la operacion: " << total_time << "segundos" << endl;
+	cout << "Tiempo promedio de la operacion: " << total_time/n << "segundos" << endl;
+
+	cout << "\nFind: \n" << endl;
+	for(unsigned long long i=0; i<n ; ++i){
+		auto start_time= chrono::high_resolution_clock::now();
+		la->find(5);
+		auto end_time = chrono::high_resolution_clock::now();
+		auto duration_time = chrono::duration_cast<chrono::microseconds>(end_time - start_time).count();
+		total_time += duration_time;
+
+	}
+
+	cout << "Tiempo promedio de la operacion: " << total_time/n << "segundos" << endl;
+
+	delete la;
 
 	return 0;
 };
