12a8184e69dfdf11db7930bcf558f6c89e19396b
daninonino <danielanovoao600@gmail.com>
2023-04-29 21:20:12 -0400

    implementacion de funcion para insertar valores random

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 
 int main(){
-//	random_device rd;
+	random_device rd;
 
 	const unsigned long long n = 10;
-	const long long maxRandValue = 2;
+	const long long maxRandValue = 3;
 	double total_time=0;
 	
 	ListArr *la = new ListArr(20);
@@ -17,40 +17,40 @@ int main(){
 	cout << "\nInsert left: \n" << endl;
 	for(unsigned long long i=0; i<n ; ++i){
 		auto start_time= chrono::high_resolution_clock::now();
-		la->insert_left(5);
+		la->insert_left(rd()%maxRandValue);
 		auto end_time = chrono::high_resolution_clock::now();
 		auto duration_time = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count();
 		total_time += duration_time;
 
 	}
 
-	cout << "Tiempo total de la operacion: " << total_time << " nanosegundos" << endl;
-	cout << "Tiempo promedio de la operacion: " << total_time/n << " nanosegundos" << endl;
+	cout << "Tiempo total de la operacion: " << total_time << "nanosegundos" << endl;
+	cout << "Tiempo promedio de la operacion: " << total_time/n << "nanosegundos" << endl;
 
 	cout << "\nInsert right: " << endl;
 	for(unsigned long long i=0; i<n ; ++i){
 		auto start_time= chrono::high_resolution_clock::now();
-		la->insert_right(5);
+		la->insert_right(rd()%maxRandValue);
 		auto end_time = chrono::high_resolution_clock::now();
 		auto duration_time = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count();
 		total_time += duration_time;
 
 	}
 
-	cout << "Tiempo total de la operacion: " << total_time << " nanosegundos" << endl;
-	cout << "Tiempo promedio de la operacion: " << total_time/n << " nanosegundos" << endl;
+	cout << "Tiempo total de la operacion: " << total_time << "nanosegundos" << endl;
+	cout << "Tiempo promedio de la operacion: " << total_time/n << "nanosegundos" << endl;
 
 	cout << "\nFind: \n" << endl;
 	for(unsigned long long i=0; i<n ; ++i){
 		auto start_time= chrono::high_resolution_clock::now();
-		la->find(5);
+		la->find(rd()%maxRandValue);
 		auto end_time = chrono::high_resolution_clock::now();
 		auto duration_time = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count();
 		total_time += duration_time;
 
 	}
 
-	cout << "Tiempo promedio de la operacion: " << total_time/n << " nanosegundos" << endl;
+	cout << "Tiempo promedio de la operacion: " << total_time/n << "nanosegundos" << endl;
 
 	delete la;
 
